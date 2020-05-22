3c027ae67a062296daa8d8cd06e6adc222840875
YanZeng <zengyan16@mails.ucas.edu.cn>
2020-05-22 13:43:25 +0800

    ssh_reconnect.sh, separate tunnel number

--- usr_local_sbin/ssh_reconnect.sh
+++ usr_local_sbin/ssh_reconnect.sh
@@ -3,6 +3,7 @@
 # IP or domain name
 oppositeIP="a.y.to"
 oppositeTunnelIP="1.11.1.8"
+tunnel="-w0:0"
 
 echo "testing ping ..."
 ping -c 3 $oppositeTunnelIP 2>/dev/null 1>/dev/null
@@ -10,6 +11,6 @@ ping -c 3 $oppositeTunnelIP 2>/dev/null 1>/dev/null
 if [ $? -eq 0  ];then
     echo "ssh is already connected and stable!"
 else
-    ssh  -fCN -w0:0 $oppositeIP
-    echo "`date` ali.yzeng1995.top, ssh tunnel is ready to restart and reconnect" >> /var/log/ssh_reconnect.log
+    ssh  -fCN $tunnel $oppositeIP
+    echo "`date` $oppositeIP, ssh tunnel is restarted and reconnected" >> /var/log/ssh_reconnect.log
 fi
