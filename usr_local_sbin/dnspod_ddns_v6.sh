3693a6181959aab947e802ef41319e6fd533fff7
Yan Zeng <zengyan16@mails.ucas.edu.cn>
2020-11-23 02:32:19 +0800

    1.Remove the old version ddns of jq. 2.Add new ipv6 version ddns and ssh_connect, and modify the original ipv4 version of curl -4

--- usr_local_sbin/dnspod_ddns_v6.sh
+++ usr_local_sbin/dnspod_ddns_v6.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# This program establish ddns service
+# This program establish ddns service for ipv6
 # to modify the public ip address of dns server.
 # The dns server is dnspod.
 # /etc/crontab # if centos etc.
-# */10 * * * * root /usr/local/sbin/dnspod_ddns.sh 1>&2> /dev/null
+# */10 * * * * root /usr/local/sbin/dnspod_ddns_v6.sh 1>&2> /dev/null
 # /etc/crontabs/root # if openwrt etc.
-# */10 * * * * /usr/sbin/dnspod_ddns.sh
+# */10 * * * * /usr/sbin/dnspod_ddns_v6.sh
 
-TOKEN="1,44e"
+TOKEN="1,af"
 DOMAIN="y5.p"
 SUB_DOMAIN="test"
 
@@ -17,8 +17,8 @@ SUB_DOMAIN="test"
 # SUB_DOMAIN=`cat dnspod_ddns.conf|grep SUB_DOMAIN|awk -F'=' '{print $2}'`
 
 
-DATA="login_token=$TOKEN&format=json&domain=$DOMAIN&sub_domain=$SUB_DOMAIN&record_type=A&offset=0&length=3"
-JOSN_RECORDS=`curl -4 -s -X POST https://dnsapi.cn/Record.List -d $DATA`
+DATA="login_token=$TOKEN&format=json&domain=$DOMAIN&sub_domain=$SUB_DOMAIN&record_type=AAAA&offset=0&length=3"
+JOSN_RECORDS=`curl -s -X POST https://dnsapi.cn/Record.List -d $DATA`
 
 RECORD_ID=`echo $JOSN_RECORDS|sed '/id/ s/.*id":"\(.*\)","ttl.*/\1/'`
 LINE_ID=`echo $JOSN_RECORDS|sed '/line_id/ s/.*line_id":"\(.*\)","type.*/\1/'`
@@ -27,12 +27,12 @@ IP_RESOLVED=`echo $JOSN_RECORDS|sed '/value/ s/.*value":"\(.*\)","enabled.*/\1/'
 
 #================get real ip
 #REAL_IP=`cat</dev/tcp/ns1.dnspod.net/6666`
-REAL_IP=`curl -4 -s  myip.ipip.net|awk -F' |：' '{print $3}'`
+REAL_IP=`curl -6 -s  http://v6.ipv6-test.com/api/myip.php`
 
 #REAL_IP="2.2.3.2"
 if [ $REAL_IP != $IP_RESOLVED ]; then
 	#================modify record
-	DATA="login_token=$TOKEN&format=json&domain=$DOMAIN&record_id=$RECORD_ID&sub_domain=$SUB_DOMAIN&value=$REAL_IP&record_type=A&record_line_id=$LINE_ID"
+	DATA="login_token=$TOKEN&format=json&domain=$DOMAIN&record_id=$RECORD_ID&sub_domain=$SUB_DOMAIN&value=$REAL_IP&record_type=AAAA&record_line_id=$LINE_ID"
 	# echo $DATA
 	JSON_RESULT=`curl -4 -s -X POST https://dnsapi.cn/Record.Modify -d $DATA`
 	echo $JSON_RESULT >> /var/log/dnspod_ddns.log
--- usr_local_sbin/ssh_reconnect_v6.sh
+++ usr_local_sbin/ssh_reconnect_v6.sh
@@ -2,7 +2,7 @@
 
 # usage:
 # /etc/crontab
-# */2 * * * * root /usr/local/sbin/ssh_reconnect.sh 1>&2> /dev/null
+# */2 * * * * root /usr/local/sbin/ssh_reconnect_v6.sh 1>&2> /dev/null
 
 # IP or domain name
 oppositeIP="a.y.to"
@@ -16,7 +16,7 @@ if [ $? -eq 0  ];then
     echo "ssh is already connected and stable!"
 else
 	ps -ef |grep ssh|grep fCN|grep " $tunnel "|awk -F' ' '{print $2}'|xargs -I {} kill {}
-    ssh -4 -fCN $tunnel $oppositeIP
+    ssh -6 -fCN $tunnel $oppositeIP
     echo "`date` $oppositeIP, ssh tunnel is restarted and reconnected" >> /var/log/ssh_reconnect.log
 fi
 
